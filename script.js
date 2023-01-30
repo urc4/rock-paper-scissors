8d9f1bf3f0bbc5e2ebdb9d92ea1b51b345908526
urc4 <121357043+urc4@users.noreply.github.com>
2023-01-30 20:59:32 -0300

    Improve game interface
    Added some extra functionalities.

--- script.js
+++ script.js
@@ -25,19 +25,28 @@ function playRound(playerSelection, computerSelection) {
 }
 
 //add class list toglle when someone reaches the desired score to change interface
-
+//add transition to interface
+//can i use create attribute here?
 const interface = document.querySelector(".interface");
-const scorePlayer = document.createElement("p");
-const scoreComputer = document.createElement("p");
+const playerDisplay = document.querySelector("#player.jock");
+const computerDisplay = document.querySelector("#computer.jock");
+const scorePlayer = document.createElement("div");
+const scoreComputer = document.createElement("div");
 
 function calculateScore() {
   if (interface.textContent.includes("win")) {
-    scorePlayer.textContent = `Playuh score ${++playerScore}`;
+    scorePlayer.textContent = `${++playerScore}`;
   } else if (interface.textContent.includes("lose")) {
-    scoreComputer.textContent = `CPU score ${++computerScore}`;
+    scoreComputer.textContent = `${++computerScore}`;
   }
-  interface.appendChild(scorePlayer);
-  interface.appendChild(scoreComputer);
+
+  displayStyle =
+    "color: #1db954; font-size: 4rem; font-weight: 900; padding: 10rem 0;";
+  scorePlayer.style.cssText = displayStyle;
+  scoreComputer.style.cssText = displayStyle;
+
+  playerDisplay.appendChild(scorePlayer);
+  computerDisplay.appendChild(scoreComputer);
 }
 
 const buttons = document.querySelectorAll("button");
@@ -48,5 +57,20 @@ buttons.forEach((button) => {
     const computerSelection = getComputerChoice();
     interface.textContent = playRound(playerSelection, computerSelection);
     calculateScore();
+    const playerDisplay = document.querySelector("#player.jock");
+    const computerDisplay = document.querySelector("#computer.jock");
+    const optionImagePlayer = document.createElement("div");
+    const optionImageComputer = document.createElement("div");
+    const playerImg = document.createElement("img");
+    const computerImg = document.createElement("img");
+    playerImg.src = `./icons/${playerSelection}.png`;
+    computerImg.src = `./icons/${computerSelection}.png`;
+    styleFlex = "display:flex;justify-content:center;";
+    optionImagePlayer.append(playerImg);
+    optionImageComputer.append(computerImg);
+    optionImagePlayer.style.cssText = styleFlex;
+    optionImageComputer.style.cssText = styleFlex;
+    playerDisplay.append(optionImagePlayer);
+    computerDisplay.append(optionImageComputer);
   });
 });
