433c6a088ae78c3a554b8dc50d13ab3f180a113e
urc4 <121357043+urc4@users.noreply.github.com>
2023-01-30 10:40:14 -0300

    Refactor code logic
    Since we depend now on the user input directly from the button
    click event the rest of the code is rendered useless. There is
    a small issue with it: figure out a way to get return values from
    event listeners.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const GAME_OPTIONS = ["rock", "paper", "scissors"];
 function getComputerChoice() {
+  const GAME_OPTIONS = ["rock", "paper", "scissors"];
   return GAME_OPTIONS[Math.floor(Math.random() * 2.999999999999)];
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const playerSelection = button.id;
+    const computerSelection = getComputerChoice();
+    alert(playRound(playerSelection, computerSelection));
+  });
+});
 
 function playRound(playerSelection, computerSelection) {
   const playerWinMessage = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
@@ -18,43 +27,4 @@ function playRound(playerSelection, computerSelection) {
   } else {
     return computerSelection === "rock" ? playerWinMessage : playerLoseMessage;
   }
-}
-
-function getPlayerChoice() {
-  let valid_choice = false;
-  let playerChoice;
-  while (!valid_choice) {
-    playerChoice = prompt("Choose ROCK, PAPER or SCISSORS", "Say my name");
-    valid_choice = GAME_OPTIONS.includes(playerChoice.toLowerCase());
-    if (!valid_choice) {
-      alert(`${playerChoice} is not allowed!!!`);
-    }
-  }
-  return playerChoice;
-}
-
-function game_on() {
-  let playerScore = 0;
-  let computerScore = 0; // account for ties now
-  let playerSelection;
-  let computerSelection;
-  for (let roundCount = 1; roundCount <= 5; roundCount++) {
-    console.log(`Round ${roundCount}`);
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    endRoundMessage = playRound(playerSelection, computerSelection);
-    endRoundMessage.includes("win") ? playerScore++ : computerScore++;
-    alert(endRoundMessage);
-  }
-  playerScore > computerScore
-    ? console.log("You won the game to 5!!!")
-    : console.log("You lost the game to 5!!!");
-}
-
-let keepPlaying = true;
-while (keepPlaying) {
-  game_on();
-  keepPlaying = confirm(
-    "Do you want to play another best to 5 game of ROCK, PAPER OR SCISSORS?"
-  );
 }
